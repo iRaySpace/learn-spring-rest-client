6be7f564c43ae9a23422df520cde67c3e2cc364f
irayspace <irayspacii@gmail.com>
2026-07-15 22:08:11 +0800

    feat(orchestrator): logger

--- orchestrator/src/main/java/com/example/orchestrator/OrchestratorController.java
+++ orchestrator/src/main/java/com/example/orchestrator/OrchestratorController.java
@@ -2,6 +2,8 @@ package com.example.orchestrator;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,15 @@ import com.example.orchestrator.payment.PaymentService;
 @RequestMapping("/v1/orchestrator")
 public class OrchestratorController {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrchestratorController.class);
+
     @Autowired
     PaymentService paymentService;
 
     @PostMapping("/order")
     Order createOrder() {
         PaymentRespDto paymentRespDto = paymentService.createPayment();
-        System.out.println("paymentRespDto: " + paymentRespDto);
+        logger.info("{}", paymentRespDto);
 
         String id = "ord_" + UUID.randomUUID().toString().substring(0, 8);
         Order order = new Order(id, paymentRespDto.getId(), "completed");
